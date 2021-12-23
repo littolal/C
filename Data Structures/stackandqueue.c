3d9f3e4a656fd8e078ca37a4f309c107f2339b03
Litto K Lal <littoklal@gmail.com>
2021-12-23 19:49:18 +0530

    Update stackandqueue.c

--- Data Structures/stackandqueue.c
+++ Data Structures/stackandqueue.c
@@ -1,37 +1,87 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define SIZE 100
-void st()
-void qu()
+#define size 100
+void st();
+void qu();
 void main()
 {
     int cho;
     do{
-        printf("Enter required action: 1.Stack 2.Queue 3.Exit\n")
+        printf("Enter required action: 1.Stack 2.Queue 3.Exit\n");
         scanf("%d",&cho);
         switch(cho)
         {
             case 1:st();
                     break;
             case 2:qu();
-                    break:
+                    break;
             case 3:exit(0);
             default:printf("Invalid entry\n");
                     break;
         }
-    }while(cho!=3)
+    }while(cho!=3);
 }
 //Function for stack implementation
 void st()
 {
-    int ch;
+    int ch,v;
     int top=-1;
+    int a[size];
     do{
-        printf("Choose Action: 1.PUSH 2.POP 3.DISPLAY 4.EXIT\n");
-        scanf("%d",ch);
+        printf("Choose Action: 1.Push 2.Pop 3.Display 4.Exit\n");
+        scanf("%d",&ch);
         switch(ch)
         {
-            case 1:
+            case 1:{ if(top==size-1)
+                        printf("Stack overflow\n");
+                    else
+                    {
+                        printf("Enter value for pushing:\n");
+                        scanf("%d",&v);
+                        top++;
+                        a[top]=v;
+                    }
+                    break;
+                    }
+            case 2:{
+                    if(top==1)
+                        printf("Stack underflow\n");
+                    else
+                    {
+                        printf("Element %d at position %d popped\n",a[top],top);
+                        top--;
+                    }
+                    break;
+                    }
+            case 3: {
+                    if(top==-1)
+                        printf("Stack is empty\n");
+                    else
+                    {
+                        printf("Stack:\n");
+                        for(int i=top;i>=0;i--)
+                        {
+                            printf("%d\n",a[i]);
+                        }
+                    }
+                    break;
+                    }
+            case 4:break;
+            default:{printf("Invalid option\n");
+                    break;
+                    }
         }
+    }while(ch!=4);
+}
+//Function for queue implementation
+void qu()
+{
+    
+    int ch,v;
+    int f=0;
+    int r=0;
+    int a[size];
+    do{
+        printf("Choose Action: 1.Insertion 2.Deletion 3.Display 4.Exit\n");
     }
 }
